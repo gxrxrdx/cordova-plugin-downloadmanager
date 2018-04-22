d28de0db3e5b3c00e090d80acadc72171a45ebf0
gxrxrdx <gxrxrdx@users.noreply.github.com>
2018-04-22 18:11:00 -0500

    Add files via upload

--- src/android/DownloadManager.java
+++ src/android/DownloadManager.java
@@ -3,6 +3,7 @@ package downloadmanager;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Environment;
+import android.util.Log;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
@@ -22,41 +23,42 @@ public class DownloadManager extends CordovaPlugin {
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("download")) {
-            String message = args.getString(0);
-            this.startDownload(message, callbackContext);
+            String descargaURL = args.getString(0);
+            String nombreArchivo = args.getString(1);
+            this.startDownload(downloadURL, nombreArchivo, callbackContext);
             return true;
         }
         return false;
     }
 
-    private void startDownload(String message, CallbackContext callbackContext) {
-        if (message != null && message.length() > 0) {
-            String filename = message.substring(message.lastIndexOf("/")+1, message.length());
-            try {
-                filename = URLDecoder.decode(filename,"UTF-8");
-            } catch (UnsupportedEncodingException e) {
-
-                callbackContext.error("Error in converting filename");
+    private void startDownload(final String message,final String nombreArchivo,final CallbackContext callbackContext) {
+        cordova.getThreadPool().execute(new Runnable() {
+            public void run() {
+                if (message != null && message.length() > 0) {                    
+                    android.app.DownloadManager downloadManager = (android.app.DownloadManager) cordova.getActivity().getApplicationContext().getSystemService(Context.DOWNLOAD_SERVICE);            
+                    Uri Download_Uri = Uri.parse("https://segura.mx/segura.jpg"/*message*/);
+                    Log.i("CORDOVA","---------------------------------");
+                    Log.i("CORDOVA","URI DESCARGA: "+Download_Uri.toString());
+                    Log.i("CORDOVA","---------------------------------");
+                    android.app.DownloadManager.Request request = new android.app.DownloadManager.Request(Download_Uri);
+                    //Restrict the types of networks over which this download may proceed.
+                    request.setAllowedNetworkTypes(android.app.DownloadManager.Request.NETWORK_WIFI | android.app.DownloadManager.Request.NETWORK_MOBILE);
+                    //Set whether this download may proceed over a roaming connection.
+                    request.setAllowedOverRoaming(false);
+                    //Set the title of this download, to be displayed in notifications (if enabled).
+                    request.setTitle(nombreArchivo);
+                    //Set a description of this download, to be displayed in notifications (if enabled)
+                    request.setDescription("DataSync File Download.");
+                    //Set the local destination for the downloaded file to a path within the application's external files directory            
+                    request.setDestinationInExternalPublicDir(cordova.getActivity().getApplicationContext(), Environment.DIRECTORY_DOWNLOADS, nombreArchivo);
+                    //Set visiblity after download is complete
+                    request.setNotificationVisibility(android.app.DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                    long downloadReference = downloadManager.enqueue(request);
+                    callbackContext.success(message);
+                } else {
+                    callbackContext.error("Expected one non-empty string argument.");
+                }
             }
-            android.app.DownloadManager downloadManager = (android.app.DownloadManager) cordova.getActivity().getApplicationContext().getSystemService(Context.DOWNLOAD_SERVICE);            
-            Uri Download_Uri = Uri.parse(message);
-            android.app.DownloadManager.Request request = new android.app.DownloadManager.Request(Download_Uri);
-            //Restrict the types of networks over which this download may proceed.
-            request.setAllowedNetworkTypes(android.app.DownloadManager.Request.NETWORK_WIFI | android.app.DownloadManager.Request.NETWORK_MOBILE);
-            //Set whether this download may proceed over a roaming connection.
-            request.setAllowedOverRoaming(false);
-            //Set the title of this download, to be displayed in notifications (if enabled).
-            request.setTitle(filename);
-            //Set a description of this download, to be displayed in notifications (if enabled)
-            request.setDescription("DataSync File Download.");
-            //Set the local destination for the downloaded file to a path within the application's external files directory            
-            request.setDestinationInExternalFilesDir(cordova.getActivity().getApplicationContext(), Environment.DIRECTORY_DOWNLOADS, filename);
-            //Set visiblity after download is complete
-            request.setNotificationVisibility(android.app.DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
-            long downloadReference = downloadManager.enqueue(request);
-            callbackContext.success(message);
-        } else {
-            callbackContext.error("Expected one non-empty string argument.");
-        }
+        });        
     }
 }
